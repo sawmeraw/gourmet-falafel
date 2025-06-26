c76f1b2d7104c8bcd5e1da77328a85c6d671577d
sawmeraw <pandeysameer96@gmail.com>
2025-06-26 18:42:25 +0930

    gallery updated

--- gourmet-falafel/src/app/page.tsx
+++ gourmet-falafel/src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
         <Hero />
         <LocationBanner/>
         <Menu />
-        <FeaturedDishes/>
+        {/* <FeaturedDishes/> */}
         <Gallery/>
         <OpeningHours/>
       </main>
